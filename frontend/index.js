e6c7510fd9900964ad727335f73713aefc31d228
SonSeungGyu <thstmd0705@naver.com>
2023-08-10 10:59:01 +0900

    로그인 인증 기능 구현

--- frontend/index.js
+++ frontend/index.js
@@ -42,8 +42,8 @@ const renderData = (data) => {
     InfoPriceDiv.className = "item-list__info-price";
     InfoPriceDiv.innerText = obj.price;
 
-    // const div = document.createElement("div");
-    // div.innerText = obj.title;
+    const div = document.createElement("div");
+    div.innerText = obj.title;
 
     imgDiv.appendChild(img);
 
@@ -58,7 +58,19 @@ const renderData = (data) => {
 };
 
 const fetchList = async () => {
-  const res = await fetch("/items");
+  const accessToken = window.localStorage.getItem("token");
+  const res = await fetch("/items", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (res.status === 401) {
+    alert("로그인이 필요합니다!");
+    window.location.pathname = "/login.html";
+    return;
+  }
+
   const data = await res.json();
   renderData(data);
 };
